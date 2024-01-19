f21080a87550850514b8925d0a322b2cf79e1dc7
pawelmikiciuk <pawelmikiciuk@gmail.com>
2024-01-19 21:23:50 +0100

    Route and Navigate

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import styles from './Sidebar.module.css';
+import Logo from './Logo';
+import AppNav from './AppNav';
+import { Outlet } from 'react-router-dom';
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <Outlet />
+
+      <footer className={styles.footer}></footer>
+      <p className={styles.copyright}>
+        &copy; Copyright {new Date().getFullYear()} by WorldWisie Inc.
+      </p>
+    </div>
+  );
+}
+
+export default Sidebar;
